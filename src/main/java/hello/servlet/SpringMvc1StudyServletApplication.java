389fe0946516406f0d22ff8d20f2866b22f14089
장정우 <jungwoo@lotte.net>
2024-03-13 19:21:06 +0900

    first commit

---
+++ src/main/java/hello/servlet/SpringMvc1StudyServletApplication.java
@@ -0,0 +1,15 @@
+package hello.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class SpringMvc1StudyServletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvc1StudyServletApplication.class, args);
+    }
+
+}
