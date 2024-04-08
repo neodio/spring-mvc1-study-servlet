11389b1775de367694f859bdd09d329d594e94e6
장정우 <enterfive@gmail.com>
2024-04-08 19:15:01 +0900

    20240408 개발

--- src/main/java/hello/servlet/SpringMvc1StudyServletApplication.java
+++ src/main/java/hello/servlet/SpringMvc1StudyServletApplication.java
@@ -11,5 +11,4 @@ public class SpringMvc1StudyServletApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringMvc1StudyServletApplication.class, args);
     }
-
 }
